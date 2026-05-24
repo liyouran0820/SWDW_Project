7697a0fba051fb001de4e11122dbf12997642f80
liyouran0820 <18724300808@163.com>
2026-05-24 19:44:36 +0800

    添加总购买表

--- total.php
+++ total.php
@@ -42,6 +42,42 @@ $message = "";
         </nav>
 
         <div id="outline">
+            <?php
+            $sql = "SELECT * FROM purchase";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo "<table id='imgTable' border='1'>";
+
+                // 显示列标题
+                $row = $result->fetch_assoc();
+                echo "<tr>";
+                foreach ($row as $key => $value) {
+                    echo "<th>" . htmlspecialchars($key) . "</th>";
+                }
+                echo "</tr>";
+
+                // 显示第一行数据
+                echo "<tr>";
+                foreach ($row as $value) {
+                    echo "<td>" . htmlspecialchars($value) . "</td>";
+                }
+                echo "</tr>";
+
+                // 显示剩余行数据
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    foreach ($row as $value) {
+                        echo "<td>" . htmlspecialchars($value) . "</td>";
+                    }
+                    echo "</tr>";
+                }
+
+                echo "</table>";
+            } else {
+                echo "No purchase records found.";
+            }
+            ?>
         </div>
     </div>
 
