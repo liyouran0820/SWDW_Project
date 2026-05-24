1827285c41143523c647c00e11dcf8b18600422e
liyouran0820 <18724300808@163.com>
2026-05-24 19:23:25 +0800

    管理员界面删除其他

--- administrator_page.php
+++ administrator_page.php
@@ -3,50 +3,6 @@ session_start();
 include "connection_bcrab.php";
 
 $message = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    if (!isset($_SESSION["username"])) {
-        echo "<script>
-            alert('Please login first.');
-            window.location.href = 'login.php';
-            </script>";
-        exit();
-    } else {
-
-        $ancient_shirt = $_POST["ancientShirt"];
-        $cap = $_POST["cap"];
-        $culture_shirt = $_POST["cultureShirt"];
-        $polo_shirt = $_POST["poloShirt"];
-
-        $sql1 = "UPDATE purchase 
-                SET quantity = quantity + $ancient_shirt 
-                WHERE itemN = 'ancientShirt'";
-
-        $sql2 = "UPDATE purchase 
-                SET quantity = quantity + $cap 
-                WHERE itemN = 'cap'";
-
-        $sql3 = "UPDATE purchase 
-                SET quantity = quantity + $culture_shirt 
-                WHERE itemN = 'cultureShirt'";
-
-        $sql4 = "UPDATE purchase 
-                SET quantity = quantity + $polo_shirt 
-                WHERE itemN = 'poloShirt'";
-
-        if (
-            $conn->query($sql1) === TRUE &&
-            $conn->query($sql2) === TRUE &&
-            $conn->query($sql3) === TRUE &&
-            $conn->query($sql4) === TRUE
-        ) {
-            $message = "Purchase submitted successfully.";
-        } else {
-            $message = "Purchase failed: " . $conn->error;
-        }
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -88,49 +44,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </nav>
 
         <div id="outline">
-            <form action="administrator_page.php" method="POST">
-
-                <table id="imgTable">
-                    <tr>
-                        <td><img src="images/clothes/ancientShirt.png"></td>
-                        <td>ancient<br>Unit Price: 100</td>
-                        <td>
-                            Quantity (0 to 9):
-                            <input name="ancientShirt" type="number" min="0" max="9" value="0" required>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td><img src="images/clothes/cap.png"></td>
-                        <td>cap<br>Unit Price: 20</td>
-                        <td>
-                            Quantity (0 to 9):
-                            <input name="cap" type="number" min="0" max="9" value="0" required>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td><img src="images/clothes/cultureShirt.png"></td>
-                        <td>cultureShirt<br>Unit Price: 50</td>
-                        <td>
-                            Quantity (0 to 9):
-                            <input name="cultureShirt" type="number" min="0" max="9" value="0" required>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td><img src="images/clothes/poloShirt.png"></td>
-                        <td>poloShirt<br>Unit Price: 60</td>
-                        <td>
-                            Quantity (0 to 9):
-                            <input name="poloShirt" type="number" min="0" max="9" value="0" required>
-                        </td>
-                    </tr>
-                </table>
-
-                <button class="button" type="submit" name="purchaseSubmit">Submit</button>
-                <button class="button" type="reset">Reset</button>
-            </form>
         </div>
     </div>
 
